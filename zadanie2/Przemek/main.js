d1fe7ae129bfa633947f0aa5eae5d53a62e7748e
Prze Mysław <przemo.cia@gmail.com>
2019-02-17 00:46:06 +0100

    rozwiązanie zadanie2 v3

--- zadanie2/Przemek/main.js
+++ zadanie2/Przemek/main.js
@@ -1,18 +1,16 @@
 var intervalId;
 var changePosition = addPosition(0);
-<<<<<<< HEAD
 var firstSpan = document.getElementById('firstSpan');
 var secondSpan = document.getElementById('secondSpan');
 var textWidth = firstSpan.offsetWidth;
-var textHalfWidth = textWidth / 2;
 
 function addPosition(reference) {
     return function (argument) {
         function setPosition() {
             if(secondSpan.offsetLeft >= textWidth)
-                reference = reference - textHalfWidth;
+                reference = reference - textWidth;
             else if (secondSpan.offsetLeft < 0)
-                reference = reference + textHalfWidth;
+                reference = reference + textWidth;
 
             secondSpan.style.left = (reference += argument) + 'px';
             firstSpan.style.left = ((reference += argument)-textWidth) + 'px';
@@ -26,18 +24,6 @@ function addPosition(reference) {
         } else {
             clearInterval(intervalId);
             setPosition();
-=======
-var elementMoveOn = document.getElementById('moveIt');
-function addPosition(reference) {
-    return function (argument) {
-        if (argument !== 0) {
-            intervalId = setInterval(function() {
-                elementMoveOn.style.left = (reference += argument) + 'px';
-            }, 50);
-        } else {
-            clearInterval(intervalId);
-            elementMoveOn.style.left = (reference += argument) + 'px';
->>>>>>> 3e183b24fabb96e82407c7047dfe20389ec251a0
         }
     }
 }
