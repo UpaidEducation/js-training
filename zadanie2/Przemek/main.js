6ca206fe6bf74a06ba1b5cb9b89adad13f13b548
pskaczukowski <przemyslaw.skaczukowski@upaid.pl>
2019-02-18 19:39:39 +0100

    rozwiązanie zadanie2 v4

--- zadanie2/Przemek/main.js
+++ zadanie2/Przemek/main.js
@@ -1,29 +1,12 @@
+var textBox = document.getElementById('textBox');
+var text = "sample text ";
+var array = text.split('');
 var intervalId;
-var changePosition = addPosition(0);
-var firstSpan = document.getElementById('firstSpan');
-var secondSpan = document.getElementById('secondSpan');
-var textWidth = firstSpan.offsetWidth;
-
-function addPosition(reference) {
-    return function (argument) {
-        function setPosition() {
-            if(secondSpan.offsetLeft >= textWidth)
-                reference = reference - textWidth;
-            else if (secondSpan.offsetLeft < 0)
-                reference = reference + textWidth;
-
-            secondSpan.style.left = (reference += argument) + 'px';
-            firstSpan.style.left = ((reference += argument)-textWidth) + 'px';
-        }
-
-        if (argument !== 0) {
-            intervalId = setInterval(function() {
-                setPosition();
-
-            }, 50);
-        } else {
-            clearInterval(intervalId);
-            setPosition();
-        }
-    }
+textBox.innerText = text;
+function changePosition(direction) {
+    clearInterval(intervalId);
+    intervalId = setInterval(function () {
+        array.push.apply(array, array.splice(0, direction ? text.length-1 : 1));
+        textBox.innerText = array.join('');
+    }, 50);
 }
