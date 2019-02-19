bc1386af6766c6519b07155587a4e1d4b3ecbf23
pskaczukowski <przemyslaw.skaczukowski@upaid.pl>
2019-02-19 12:56:44 +0100

    rozwiązanie zadanie2 v4

--- zadanie2/Przemek/main.js
+++ zadanie2/Przemek/main.js
@@ -2,11 +2,10 @@ var intervalId;
 var textBox = document.getElementById('textBox');
 var array = textBox.innerText.split('');
 array.push('\xa0');
-array.join('');
 function changePosition(direction) {
     clearInterval(intervalId);
     intervalId = setInterval(function () {
         array.push.apply(array, array.splice(0, direction ? array.length-1 : 1));
         textBox.innerText = array.join('');
-    }, 50);
+    }, 1000);
 }
