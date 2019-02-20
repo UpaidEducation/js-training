971f04b6e0e8994e4aca91238ee330b12f587a79
kate <gdakaska@gmail.com>
2019-02-20 11:15:56 +0100

    Zadanie 2, nieudolna próba rozwiązania w alternatywny sposób

--- zadanie2/Katarzyna/script.js
+++ zadanie2/Katarzyna/script.js
@@ -1,10 +1,25 @@
-var animatedText = document.getElementById("animated-text");
-function changeAnimation() {
-    if (animatedText.classList[0] === "scroll-right") {
-        animatedText.classList.remove("scroll-right");
-        animatedText.classList.add("scroll-left");
-    } else {
-        animatedText.classList.remove("scroll-left");
-        animatedText.classList.add("scroll-right");
+var animatedText = 'sample text ';
+document.getElementById('app-text').innerHTML = animatedText;
+
+function moveLeft(animatedText) {
+    var newText = newText + animatedText.charAt(0);
+
+
+    if (animatedText === '') {
+
     }
+
+    animatedText = animatedText.substr(1, animatedText.length);
+
+
+    console.log(animatedText.substr(1, animatedText.length));
+
+    document.getElementById('app-text').innerHTML = animatedText + newText;
+
+    return moveLeft(animatedText.substr(1, animatedText.length) + animatedText.charAt(0));
+
+}
+
+function moveRight(animatedText) {
+    animatedText = document.getElementById('app-text').innerHTML;
 }
