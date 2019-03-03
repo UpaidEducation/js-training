777d3b2ef698248c808e027fd90cbb104a181222
Prze Mysław <przemo.cia@gmail.com>
2019-03-03 17:06:53 +0100

    rozwiązanie zadanie5

--- zadanie5/Przemek/main.js
+++ zadanie5/Przemek/main.js
@@ -1,14 +1,14 @@
 /*
 * Function 1
 */
-const dateInput = document.getElementById('date');
-const sortStr = document.getElementById('sortString');
+const dateInput = document.getElementById('dateInput').valueOf();
+
 function timestampToDate() {
     let date = new Date(+dateInput.value);
     let dateResult=[];
     let regex = /^\d+$/;
 
-    ['getHours', 'getMinutes', 'getSeconds', 'getMilliseconds'].forEach(function (item, index) {
+    ['getHours', 'getMinutes', 'getSeconds', 'getMilliseconds'].forEach(function(item, index) {
         dateResult[index] = date[item]();
     });
 
@@ -19,18 +19,25 @@ function timestampToDate() {
 /*
 * Function 2
 */
-let sortString = (str) => str.split('').sort().join('');
-if (sortStr.lastElementChild)
-    sortStr.lastElementChild.innerText = sortString(sortStr.firstElementChild.textContent);
+const sortStr = document.getElementById('sortString');
+const sortInput = document.getElementById('sortInput');
 
+let sortString = (str) => str.split('').sort().join('');
+function sort() {
+    if (sortStr.lastElementChild && sortInput)
+        sortStr.lastElementChild.innerText = sortString(sortInput.value);
+}
 
 /*
 * Function Advanced
 */
-function findMaxPossibleSum(array, n) {
+function findMaxPossibleSum(arrayStr, n) {
     let sum = 0;
     let arr = [];
     let sumArr = [];
+    let array = arrayStr.map(function(str) {
+        return parseInt(str);
+    });
 
     array.forEach(function(item, index) {
         arr = array.slice(index, n + index);
@@ -41,14 +48,19 @@ function findMaxPossibleSum(array, n) {
             sumArr[index] = sum;
     });
 
-    let content = Math.max.apply(null, sumArr);
-    if (array.length < n)
-        content = "This operation can not be done";
-
-    return content;
+    return arrayStr.length < n ?
+        'This operation can not be done' :
+        Math.max(...sumArr);
 }
 
-let maxSum = document.getElementById('maxSum');
-if (maxSum.lastElementChild)
-    maxSum.lastElementChild.textContent = findMaxPossibleSum([1, 2, 3, -4, 5, -6, 7, 8, -9], 4).toString();
+const maxSumInput = document.getElementById('maxSumInput');
+const nInput = document.getElementById('nInput');
+const maxSum = document.getElementById('maxSum');
 
+function calc() {
+    if (maxSum.lastElementChild)
+        maxSum.lastElementChild.textContent = findMaxPossibleSum(
+            maxSumInput.value.split(","),
+            +nInput.value
+        ).toString();
+}
