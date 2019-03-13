a36ac466ffe72af23cfbafaa252b5a55e8cd0d16
pskaczukowski <przemyslaw.skaczukowski@upaid.pl>
2019-03-13 12:32:34 +0100

    rozwiązanie zadanie6

--- zadanie6/Przemek/main.js
+++ zadanie6/Przemek/main.js
@@ -30,9 +30,41 @@ function createPalindrom() {
 const numberInput = document.getElementById('number');
 
 function countPattern(value, dividers, result) {
-    return dividers <= value ? countPattern(value, +(dividers * 2), result += value / dividers) : result;
+    return value >= dividers ? countPattern(value, parseInt(dividers * 2), result += parseInt(value / dividers)) : result;
 }
 
 function calculate() {
     numberInput.value = countPattern(+numberInput.value, 1, 0);
+}
+
+/*
+* Function 3
+*/
+
+const matrixInput = document.getElementById('matrix');
+
+let matrix =
+`[
+[1, 0, 0, 0, 0, 0], 
+[0, 3, 0, 0, 0, 0],
+[0, 0, 7, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 2, 0],
+[0, 0, 0, 0, 0, 9]
+]`;
+
+matrixInput.value = matrix;
+
+function countMatrix() {
+    let array = eval(matrixInput.value);
+    let isDiagonal = "Yes";
+
+    for (var m=0; m < array.length; m++)
+        for (var n=0; n < array[m].length; n++)
+            if (m !== n && array[m][n] !== 0) {
+                isDiagonal = "No";
+                break;
+            }
+
+    document.getElementById('isDiagonal').innerText = isDiagonal;
 }
