7178e8637f1d5dcf74b989257aafb46af0c60c58
pskaczukowski <przemyslaw.skaczukowski@upaid.pl>
2019-03-19 15:55:51 +0100

    add app

--- weatherapp/Przemek/main.js
+++ weatherapp/Przemek/main.js
@@ -1,30 +1,23 @@
+var mainBox = document.getElementById("mainBox");
 var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=bb97b3f43e59cd2ba46ca6ccf5cbad37';
 var http = new XMLHttpRequest();
-var data = {};
-var message = "";
 
-try {
-    http.open('GET', weatherUrl, false);
-    http.onreadystatechange = function() {
-        if (http.readyState == XMLHttpRequest.DONE && http.status == 200)
-            data = JSON.parse(http.responseText);
-        else if (http.readyState == XMLHttpRequest.DONE)
-            message = JSON.parse(http.responseText).message;
-    };
-    http.send();
-} catch (e) {
-    message = e.message;
+http.onreadystatechange = function() {
+    if (http.readyState === XMLHttpRequest.DONE && http.status == 200)
+        setDataWeather(JSON.parse(http.responseText))
+    else if (http.readyState === XMLHttpRequest.DONE)
+        mainBox.innerHTML = '<h4>'+ JSON.parse(http.responseText).message +'</h4>';
+};
+http.open('GET', weatherUrl);
+http.send();
+
+function setDataWeather(data) {
+    mainBox.children[0].lastChild.innerText = data.name;
+    mainBox.children[1].innerHTML = '<img src="http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png"/>';
+    mainBox.children[2].lastChild.innerText = data.weather[0].description;
+    mainBox.children[3].lastChild.innerText = data.main.temp + '°C';
+    mainBox.children[4].lastChild.innerText = data.main.humidity + '%';
+    mainBox.children[5].lastChild.innerText = data.main.pressure + ' hPa';
+    mainBox.children[6].lastChild.innerText = data.visibility / 1000 + ' km';
+    mainBox.children[7].lastChild.innerText = data.wind.speed + ' km/h';
 }
-
-if (message !== "") {
-    document.getElementById('mainBox').innerHTML = '<h4>'+ message +'</h4>';
-} else {
-    document.getElementById('name').lastChild.innerText = data.name;
-    document.getElementById('weather').lastChild.innerText = data.weather[0].description;
-    document.getElementById('temp').lastChild.innerText = data.main.temp + '°C';
-    document.getElementById('humidity').lastChild.innerText = data.main.humidity + '%';
-    document.getElementById('pressure').lastChild.innerText = data.main.pressure + ' hPa';
-    document.getElementById('visibility').lastChild.innerText = data.visibility / 1000 + ' km';
-    document.getElementById('speed').lastChild.innerText = data.wind.speed + ' km/h';
-}
-
