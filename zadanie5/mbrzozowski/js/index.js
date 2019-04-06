e2c2a8477d5b58aea8aff60bd0f489db7c1ee46a
Maciej Brzozowski <maciej.brzozowski.83@gmail.com>
2019-04-06 23:28:30 +0200

    task 5 part 2

--- zadanie5/mbrzozowski/js/index.js
+++ zadanie5/mbrzozowski/js/index.js
@@ -4,6 +4,29 @@ var index = (function(){
     var convertButton = document.getElementById("convert");
     var dataIn = document.getElementById("dataIn");
 
+    var resultString = document.getElementById("resultString");
+    var convertButtonString = document.getElementById("convertString");
+    var stringOut = document.getElementById("resultString");
+
+    var arrayIn = document.getElementById("arrayIn");
+    var elementsIn = document.getElementById("elementsIn");
+    var calculateButton = document.getElementById("calculate");
+    var resultSum = document.getElementById("resultSum");
+
+    calculateButton.addEventListener('click',function(){
+        var array = arrayIn.value.split(",").map(function(value){return value*1; });
+        var elements = elementsIn.value * 1;
+        if(elements > array.length){
+            resultSum.innerHTML = "This operation can not be done";
+        }else{
+            resultSum.innerHTML = findMaxPossibleSum(array,elements);
+        }
+    });
+
+    convertButtonString.addEventListener('click',function(){
+        stringOut.innerHTML = sortString(stringIn.value);
+    });
+
     convertButton.addEventListener('click',function(){
     if (!isNaN(dataIn.value)){
         result.innerHTML = convertNumberToTime(dataIn.value);
@@ -33,6 +56,19 @@ var index = (function(){
             }
     return number;
     }
+
+    function sortString(stringIn){
+        var stringOut = stringIn.split("").sort().join("");
+    return stringOut;
+    }
+
+    function findMaxPossibleSum(array, elements){
+        return array.sort(function(x,y){return x - y;})
+                    .filter(function(value){return value >= array[array.length - elements]})
+                    .reduce(function(total, number){return total + number;});
+    }
+
+    console.log(findMaxPossibleSum([1, 2, 3, 14, 5], 2));
 
 })();
 
