8a5729790b1bb295e0ba9e85619c265365162766
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-04-15 23:34:10 +0200

    Task 6 - simple calculator

--- zadanie6/mbrzozowski/js/calculator.js
+++ zadanie6/mbrzozowski/js/calculator.js
@@ -1,20 +1,47 @@
-var calculator = (function(){
+(function(){
 
-    var stringEquation="";
+    var equation="";
 
-    var container = document.getElementById('calculatorContainer');
+    var buttonContainer = document.getElementById('calculatorContainer');
+    var equationOut = document.getElementById('equationOut');
+    var calcOut = document.getElementById('calcOut');
 
-    container.addEventListener('click',handleEvent);
+    buttonContainer.addEventListener('click',eventHandler);
 
-    function handleEvent(evt){
-        if (evt.target.value){
-            console.log(evt.target.value);
+    function eventHandler(evt){
+        if(evt.target.value){
+            if (evt.target.value === 'CE'){
+                equation = '';
+                displayValue(calcOut, '0');
+                displayValue(equationOut, '');
+                return;
+            }
+            if (evt.target.value === '='){
+                var result = calculate(equation);
+                console.log(result);
+                displayValue(calcOut, result);
+                equation ='';
+            }
+            else{
+            equation = addValue(equation, evt.target.value);
+            console.log(equation);
+            displayValue(equationOut, equation);
+            }
         }
     }
 
-    function calculate(stringEquation){
-        console.log(stringEquation);
-        return 0;
+    function calculate(equation){
+        return eval(equation);
+    }
+
+    function displayValue(elementHTML, value){
+        elementHTML.innerHTML = value;
+    }
+
+    function addValue(equation,value){
+        if (value === 'del') return equation.substring(0,equation.length -1);
+        return equation + value;
+
     }
 
 })();
