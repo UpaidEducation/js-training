5a2a674782b6681b9972241ef565c95b7dbd787a
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-04-28 14:18:35 +0200

    delete callback and simplify generalCallback, add event handler for saved data

--- weatherapp/mbrzozowski/js/app.js
+++ weatherapp/mbrzozowski/js/app.js
@@ -1,9 +1,9 @@
 
 const API_KEY = 'c11cadd1b603b24d0c2b48f80ad478e5';
 
-document.getElementById('london').addEventListener('click',function(){EVT.emit('drawCityWeather','London')});
-document.getElementById('customCity').addEventListener('click',function(){EVT.emit('customCity')});
-document.getElementById('weatherChart').addEventListener('click',function(){EVT.emit('weatherChart')});
+document.getElementById('london').addEventListener('click',function(){EVT.emit('drawCityWeather','London');});
+document.getElementById('customCity').addEventListener('click',function(){EVT.emit('customCity');});
+document.getElementById('weatherChart').addEventListener('click',function(){EVT.emit('weatherChart');});
 
 (function(){
     window.EVT = new EventEmitter2();
--- weatherapp/mbrzozowski/js/request.js
+++ weatherapp/mbrzozowski/js/request.js
@@ -5,27 +5,22 @@ var PUT = 'PUT';
 var DELETE = 'DELETE';
 
  var generalCallback = {
-        _200:(function(data){}),
-        _201:(function(data){}),
-        _202:(function(data){}),
-        _204:(function(data){}),
-        _400:(function(data){}),
+        _200:(function(data){EVT.emit('saveCityData',data);}),
         _401:(function(data){alert(data.message)}),
-        _403:(function(data){}),
-        _404:(function(data){}),
-        _409:(function(data){}),
-        _412:(function(data){}),
-        _422:(function(data){}),
-        _500:(function(data){}),
+        _429:(function(data){alert(data.message)}),
     };
 
 function makeRequest(method,url,body,callback){
+var callback = callback;
 const http = new XMLHttpRequest();
 http.open(method,url,true)
 if(body){
     http.setRequestHeader('Content-Type','application/json');
 };
 
+if(!callback){
+    callback = generalCallback;
+}
 http.onreadystatechange = function() {
     if (this.readyState == 4){
         console.log('status '+this.status,http);
