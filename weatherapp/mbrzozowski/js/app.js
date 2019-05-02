6af407e2d01d70dc3860579d8c5d5025cc9aee6f
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-05-02 09:19:23 +0200

    Weather app - Handle evets to draw forecast data

--- weatherapp/mbrzozowski/js/app.js
+++ weatherapp/mbrzozowski/js/app.js
@@ -1,11 +1,11 @@
 
 const API_KEY = 'c11cadd1b603b24d0c2b48f80ad478e5';
 
-document.getElementById('london').addEventListener('click',function(){EVT.emit('drawCityWeather','London');});
+document.getElementById('london').addEventListener('click',function(){EVT.emit('drawCityWeather','London',false);});
 document.getElementById('customCity').addEventListener('click',function(){EVT.emit('customCity');});
 document.getElementById('weatherChart').addEventListener('click',function(){EVT.emit('weatherChart');});
 
 (function(){
     window.EVT = new EventEmitter2();
-    EVT.emit('getCityWeather','London');
+    EVT.emit('getCityWeather','London',false);
 })();
--- weatherapp/mbrzozowski/js/request.js
+++ weatherapp/mbrzozowski/js/request.js
@@ -3,14 +3,18 @@ var POST = 'POST';
 var GET = 'GET';
 var PUT = 'PUT';
 var DELETE = 'DELETE';
+var isForecast =false;
 
  var generalCallback = {
-        _200:(function(data){EVT.emit('saveCityData',data);}),
+        _200:(function(data){   console.log(isForecast);
+                                EVT.emit('saveCityData',data,isForecast);}),
+        _400:(function(data){alert(data.message)}),
         _401:(function(data){alert(data.message)}),
+        _404:(function(data){alert(data.message)}),
         _429:(function(data){alert(data.message)}),
     };
 
-function makeRequest(method,url,body,callback){
+function makeRequest(method,url,body,callback,forecast){
 var callback = callback;
 const http = new XMLHttpRequest();
 http.open(method,url,true)
@@ -21,6 +25,7 @@ if(body){
 if(!callback){
     callback = generalCallback;
 }
+isForecast = forecast;
 http.onreadystatechange = function() {
     if (this.readyState == 4){
         console.log('status '+this.status,http);
