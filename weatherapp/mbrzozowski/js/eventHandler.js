be73642d17c5c9cede591aab36701f1894ddf4e2
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-05-02 09:19:23 +0200

    Weather app - Handle evets to draw forecast data

--- weatherapp/mbrzozowski/js/eventHandler.js
+++ weatherapp/mbrzozowski/js/eventHandler.js
@@ -1,31 +1,35 @@
 one_hour = 1000*60*60;
 
-EVT.on('saveCityData',function(data){
+EVT.on('saveCityData',function(data,forecast){
+    console.log(forecast);
     window.localStorage.setItem(data.name,JSON.stringify(data));
-    EVT.emit('newDataSaved',data);
+    EVT.emit('newDataSaved',data,forecast);
 });
 
-EVT.on('newDataSaved',function(data){
+EVT.on('newDataSaved',function(data,forecast){
     console.log('City data', data);
-    document.getElementById('weatherData').innerHTML = cityWeatherTemplate(data);
+    console.log('Forecast', forecast);
+    var html = forecast ? cityWeatherForecastTemplate(data) : cityWeatherTemplate(data);
+    document.getElementById('weatherData').innerHTML = html;
 });
 
-EVT.on('drawCityWeather',function(city,forecast=false){
+EVT.on('drawCityWeather',function(city,forecast){
     var cityData = JSON.parse(window.localStorage.getItem(city));
     var eventName = forecast?'getCityForecast':'getCityWeather'
     if (cityData == null || (cityData.dt*1000+one_hour < Date.now())){
-        EVT.emit(eventName,city);
+        EVT.emit(eventName,city,forecast);
     }else{
         EVT.emit('newDataSaved',cityData);
     }
 });
 
-EVT.on('getCityWeather',function(city,forecast=false){
+EVT.on('getCityWeather',function(city,forecast){
         var dataType = forecast?'forecast':'weather'
         makeRequest(GET,
             'https://api.openweathermap.org/data/2.5/'+dataType+'?q='+city+'&units=metric&appid='+API_KEY,
             null,
-            null);
+            null,
+            forecast);
         }
 );
 
@@ -37,6 +41,5 @@ EVT.on('customCity',function(){
 EVT.on('searchCity',function(){
     var city = document.getElementById('city').value;
     EVT.emit('getCityWeather',city,true);
-    EVT.emit('drawCityWeather',city,true);
 });
 
