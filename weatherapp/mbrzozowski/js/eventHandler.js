77e06074097be6dacc59607520f78ec303e8eb42
mbrzozowski <maciej.brzozowski@upaid.pl>
2019-05-02 10:07:19 +0200

    Weather app - fix bug with save forecast data

--- weatherapp/mbrzozowski/js/eventHandler.js
+++ weatherapp/mbrzozowski/js/eventHandler.js
@@ -1,24 +1,23 @@
 one_hour = 1000*60*60;
 
 EVT.on('saveCityData',function(data,forecast){
-    console.log(forecast);
-    window.localStorage.setItem(data.name,JSON.stringify(data));
+    var city = forecast ? data.city.name : data.name;
+    window.localStorage.setItem(city,JSON.stringify(data));
     EVT.emit('newDataSaved',data,forecast);
 });
 
 EVT.on('newDataSaved',function(data,forecast){
-    console.log('City data', data);
-    console.log('Forecast', forecast);
     var html = forecast ? cityWeatherForecastTemplate(data) : cityWeatherTemplate(data);
     document.getElementById('weatherData').innerHTML = html;
 });
 
 EVT.on('drawCityWeather',function(city,forecast){
     var cityData = JSON.parse(window.localStorage.getItem(city));
-    var eventName = forecast?'getCityForecast':'getCityWeather'
     if (cityData == null || (cityData.dt*1000+one_hour < Date.now())){
-        EVT.emit(eventName,city,forecast);
+        console.log("City data", cityData);
+        EVT.emit('getCityWeather',city,forecast);
     }else{
+        console.log("City data", cityData);
         EVT.emit('newDataSaved',cityData,forecast);
     }
 });
